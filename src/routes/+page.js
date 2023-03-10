969cda32d161cee1704cceb3ca23386cbcf8c144
Thomas Collardeau <tomadeau@gmail.com>
2023-03-10 22:07:53 +0100

    extract some demo stuff

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,5 @@
 export const load = async ({ fetch }) => {
-	const response = await fetch('https://dummyjson.com/products?limit=3');
+	const response = await fetch('https://dummyjson.com/products?limit=2');
 	const products = await response.json();
 	return {
 		name: 'Billy',
