4a713d691010dc5a6c588d743900bf2b148a6dfe
Thomas Collardeau <tomadeau@gmail.com>
2023-03-13 23:15:17 +0100

    fix up api post route

--- src/routes/api/gpt/+server.js
+++ src/routes/api/gpt/+server.js
@@ -2,18 +2,27 @@ import { json } from '@sveltejs/kit';
 import gpt from '$lib/openai/chatgpt';
 import { addItem, getItems } from '$lib/db/mock.js';
 
+// Handle HTTP POST requests
 export const POST = async ({ request }) => {
 	try {
-		// throw new Error('Oops, something went wrong!');
+		// Get tone and content from the request's JSON body
 		const { tone, content } = await request.json();
-		await addItem({ role: 'user', content });
+		// Create system and user messages
+		const systemMsg = { role: 'system', content: `use a ${tone} tone` };
+		const lastMsg = { role: 'user', content };
+		//  Retrieve all messages from mock database
 		let msgs = await getItems();
-		msgs = [{ role: 'system', content: `use a ${tone} tone` }, ...msgs]; // maybe just put it last?
-		console.log(msgs);
+		// Add system and user messages to the beginning of the messages array
+		msgs = [systemMsg, ...msgs, lastMsg];
+		// Generate a chat response based on all messages
 		const chatResponse = await gpt(msgs);
+		// Add user message and chat response to mock database
+		await addItem(lastMsg);
 		await addItem(chatResponse);
+		// Return success response with chat response
 		return json({ success: true, chatResponse });
 	} catch (error) {
+		// Log error and return error response
 		console.log(error.message);
 		return json({ success: false, error: error.message }, { status: 500 });
 	}
